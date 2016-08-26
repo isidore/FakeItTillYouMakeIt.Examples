87683d9be3bc15f40936aaa3aca075354b1ae499
Llewellyn Falco <isidore@setgame.com>
2016-08-26 04:05:59 -0700

    Phone Number 1 - create

---
+++ FakeItTillYouMakeIt.Examples/PhoneNumberTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FakeItTillYouMakeIt.Examples
+{
+    [TestClass]
+    public class PhoneNumberTest
+    {
+        [TestMethod]
+        public void TestPrint()
+        {
+            Assert.AreEqual("+1(858)336-6560", PhoneNumber.ToString("18583366560"));
+        }
+    }
+
+    public class PhoneNumber
+    {
+        public static string ToString(String s)
+        {
+            return "+1(858)336-6560";
+        }
+    }
+}
