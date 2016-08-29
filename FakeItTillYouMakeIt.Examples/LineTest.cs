aee539887149e4093900b940dba2f87c5e2efb79
Llewellyn Falco <isidore@setgame.com>
2016-08-29 02:39:07 -0700

    Line 2

---
+++ FakeItTillYouMakeIt.Examples/LineTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FakeItTillYouMakeIt.Examples
+{
+     [TestClass]
+    public class LineTest
+    {
+         [TestMethod]
+         public void TestHorizontalLength2()
+         {
+             // Create a Line from 0,0 to 2,0
+             // verify the length is 2
+         }
+    }
+}
