5d1dc60efb517e65d73885a85d3cf9d081a1702d
Llewellyn Falco <isidore@setgame.com>
2016-08-29 02:44:14 -0700

    Fake x

--- FakeItTillYouMakeIt.Examples/LineTest.cs
+++ FakeItTillYouMakeIt.Examples/LineTest.cs
@@ -5,11 +5,44 @@ namespace FakeItTillYouMakeIt.Examples
      [TestClass]
     public class LineTest
     {
-         [TestMethod]
-         public void TestHorizontalLength2()
-         {
-             // Create a Line from 0,0 to 2,0
-             // verify the length is 2
-         }
+[TestMethod]
+public void TestHorizontalLength2()
+{
+    var line = new Line(0, 0, 2, 0);
+    Assert.AreEqual(2, line.Length);
+}
+[TestMethod]
+public void TestHorizontalLength4()
+{
+    var line = new Line(0, 0, 4, 0);
+    Assert.AreEqual(4, line.Length);
+}
+[TestMethod]
+public void TestVerticalLength4()
+{
+    var line = new Line(0, 0, 0, 4);
+    Assert.AreEqual(4, line.Length);
+}
+    }
+
+    public class Line
+    {
+        private readonly int x1;
+        private readonly int y1;
+        private readonly int x2;
+        private readonly int y2;
+
+        public Line(int x1, int y1, int x2, int 
+            y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public int Length {
+    get { return x2-x1; }
+}
     }
 }
