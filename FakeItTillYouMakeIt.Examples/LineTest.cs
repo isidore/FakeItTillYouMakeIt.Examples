a7be79f6cead9331d0403914c8bf758855f0a890
Llewellyn Falco <isidore@setgame.com>
2016-08-29 02:45:58 -0700

    x+y

--- FakeItTillYouMakeIt.Examples/LineTest.cs
+++ FakeItTillYouMakeIt.Examples/LineTest.cs
@@ -2,27 +2,29 @@
 
 namespace FakeItTillYouMakeIt.Examples
 {
-     [TestClass]
+    [TestClass]
     public class LineTest
     {
-[TestMethod]
-public void TestHorizontalLength2()
-{
-    var line = new Line(0, 0, 2, 0);
-    Assert.AreEqual(2, line.Length);
-}
-[TestMethod]
-public void TestHorizontalLength4()
-{
-    var line = new Line(0, 0, 4, 0);
-    Assert.AreEqual(4, line.Length);
-}
-[TestMethod]
-public void TestVerticalLength4()
-{
-    var line = new Line(0, 0, 0, 4);
-    Assert.AreEqual(4, line.Length);
-}
+        [TestMethod]
+        public void TestHorizontalLength2()
+        {
+            var line = new Line(0, 0, 2, 0);
+            Assert.AreEqual(2, line.Length);
+        }
+
+        [TestMethod]
+        public void TestHorizontalLength4()
+        {
+            var line = new Line(0, 0, 4, 0);
+            Assert.AreEqual(4, line.Length);
+        }
+
+        [TestMethod]
+        public void TestVerticalLength4()
+        {
+            var line = new Line(0, 0, 0, 4);
+            Assert.AreEqual(4, line.Length);
+        }
     }
 
     public class Line
@@ -32,7 +34,7 @@ public void TestVerticalLength4()
         private readonly int x2;
         private readonly int y2;
 
-        public Line(int x1, int y1, int x2, int 
+        public Line(int x1, int y1, int x2, int
             y2)
         {
             this.x1 = x1;
@@ -41,8 +43,14 @@ public void TestVerticalLength4()
             this.y2 = y2;
         }
 
-        public int Length {
-    get { return x2-x1; }
-}
+        public int Length
+        {
+            get
+            {
+                var x = x2 - x1;
+                var y = y2 - y1;
+                return x + y;
+            }
+        }
     }
 }
